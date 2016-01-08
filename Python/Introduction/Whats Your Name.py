59b262cd63fcf119a0006dceaec90cb33aa25254
Paul Adams <paul@baggerspion.net>
2016-01-08 10:48:38 +0100

    Added 'What's Your Name?' solution

---
+++ Python/Introduction/Whats Your Name.py
@@ -0,0 +1,5 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+name    = raw_input()
+surname = raw_input()
+
+print "Hello %s %s! You just delved into python." % (name, surname)
