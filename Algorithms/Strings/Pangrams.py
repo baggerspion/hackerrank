d8529f5a0712eaf1e251230a56602fa141828f65
Paul Adams <paul@baggerspion.net>
2016-01-08 12:22:39 +0100

    Added 'Pangrams' solution

---
+++ Algorithms/Strings/Pangrams.py
@@ -0,0 +1,30 @@
+s = raw_input().strip().lower()
+if ('a' in s and
+   'b' in s and
+   'c' in s and
+   'd' in s and
+   'e' in s and
+   'f' in s and
+   'g' in s and
+   'h' in s and
+   'i' in s and
+   'j' in s and
+   'k' in s and
+   'l' in s and
+   'm' in s and
+   'n' in s and
+   'o' in s and
+   'p' in s and
+   'q' in s and
+   'r' in s and
+   's' in s and
+   't' in s and
+   'u' in s and
+   'v' in s and
+   'w' in s and
+   'x' in s and
+   'y' in s and
+   'z' in s):
+    print "pangram"
+else:
+    print "not pangram"
