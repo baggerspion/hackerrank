7fe71001b517482bc2551ec4f03094c821974b41
Paul Adams <paul@baggerspion.net>
2016-01-11 15:27:55 +0100

    Improved solution to 'Sherlock and Squares'

--- Algorithms/Implementations/Sherlock And Squares.py
+++ Algorithms/Implementations/Sherlock And Squares.py
@@ -1,12 +1,9 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 # This works but is super-slow. Not sure about the faster solution.
-import math
-
-def is_square(x):
-    sqrt = int(math.sqrt(x))
-    return sqrt**2 == x
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from math import ceil, floor, sqrt
 
 t = int(raw_input())
 for _ in xrange(t):
-    min, max = map(int, raw_input().split())
-    print sum(1 for x in xrange(min, max + 1) if is_square(x))
+    min, max = map(int, raw_input().split()) 
+    print int(floor(sqrt(max)) - ceil(sqrt(min))) + 1
